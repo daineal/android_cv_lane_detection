76b0e76b956421efe2d1fe92f096b3b0edcd61b7
DanilM <murzadeneal@gmail.com>
2017-07-27 13:12:43 +0300

    edit settings

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,13 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-OPENCV_ROOT:=/home/wenfahu/NVPACK/OpenCV3-android-sdk
+OPENCV_ROOT:=/home/submarine/Загрузки/OpenCV-android-sdk
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include ${OPENCV_ROOT}/sdk/native/jni/OpenCV.mk
 
-NDK_MODULE_PATH=/home/wenfahu/NVPACK/android-sdk-linux/ndk-bundle
+NDK_MODULE_PATH=/home/submarine/Android/Sdk/ndk-bundle
 LOCAL_ARM_NEON := true
 LOCAL_SRC_FILES := com_example_wenfahu_lane_droid_MainActivity.cpp
 LOCAL_CPPFLAGS := -std=gnu++0x
---
+++ app/src/main/obj/local/arm64-v8a/objs/native/com_example_wenfahu_lane_droid_MainActivity.o.d
@@ -0,0 +1,116 @@
+obj/local/arm64-v8a/objs/native/com_example_wenfahu_lane_droid_MainActivity.o: \
+ jni/com_example_wenfahu_lane_droid_MainActivity.cpp \
+ jni/com_example_wenfahu_lane_droid_MainActivity.h \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdint \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ jni/linefinder.h \
+ /home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp
+
+jni/com_example_wenfahu_lane_droid_MainActivity.h:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdint:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/home/submarine/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+jni/linefinder.h:
+
+/home/submarine/Загрузки/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
